f573310b26758aeb39d33abcfba99634b63ad16a
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2020-11-25 00:19:09 +0100

    prevent empty values

--- statics/scripts/dataTreatment.js
+++ statics/scripts/dataTreatment.js
@@ -30,35 +30,52 @@ class DataTreatment {
         const inputX = document.createElement('input')
         inputX.type = 'number'
         inputX.name = 'x'
+        inputX.placeholder = x
+        inputX.step = 0.01
         inputX.value = x
+        inputX.required = true
         inputX.className = 'input'
-        this.changeListener(inputX)
+
+        const plus = document.createElement('p')
+        plus.textContent = '+'
 
         const inputY = document.createElement('input')
         inputY.type = 'number'
         inputY.name = 'y'
+        inputY.placeholder = y
+        inputY.step = 0.01
         inputY.value = y
+        inputY.required = true
         inputY.className = 'input'
-        this.changeListener(inputY)
+
+        const equal = document.createElement('p')
+        equal.textContent = '='
 
         const result = document.createElement('span')
         result.className = 'result'
 
-        form.append(inputX, inputY, result)
+        form.append(inputX, plus, inputY, equal, result)
         document.body.append(form)
+
+        this.changeListener(inputX, form)
+        this.changeListener(inputY, form)
     }
 
-    changeListener(element) {
-        element.addEventListener('change', e => {
-            const form = e.target.form
+    changeListener(element, form) {
+        this.findResult(form)
+        element.addEventListener('change', e => this.findResult(e.target.form))
+    }
 
+    findResult(form) {
+        const { x, y } = form.children
+
+        if (x.value && y.value)
             fetch(form.action, {
                 method: form.method,
                 body: new FormData(form)
             })
                 .then(data => data.text())
-                .then(result => element.parentElement.lastChild.textContent = result)
-        })
+                .then(result => form.lastChild.textContent = parseFloat(result).toFixed(2))
     }
 }
 
