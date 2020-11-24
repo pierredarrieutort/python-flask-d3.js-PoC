fe7f2d29d3f033815440c8b75fd97a61c8943aaa
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2020-11-24 21:57:55 +0100

    start script added

--- statics/scripts/dataTreatment.js
+++ statics/scripts/dataTreatment.js
@@ -1,4 +1,15 @@
 import 'https://cdnjs.cloudflare.com/ajax/libs/d3/6.2.0/d3.min.js'
 
+class DataTreatment {
+    constructor() {
+        this.loadData()
+    }
 
-console.log(window)
+    loadData() {
+        d3.csv(this.filepath).then(data => {
+            console.log(data)
+        })
+    }
+}
+
+new DataTreatment()
